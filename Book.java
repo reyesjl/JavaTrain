6f056adb4262b1e070d014622404d2acf839271b
Jose <reyesjl.nine@hotmail.com>
2024-02-12 02:12:43 +0000

    add Book class;

---
+++ Book.java
@@ -0,0 +1,56 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+    private boolean available; 
+
+    public Book(String title, String author, String isbn, int publicationYear, boolean avaiable) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.available = available;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public boolean getAvailable() {
+        return available;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+}
