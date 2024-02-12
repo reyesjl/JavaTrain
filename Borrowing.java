a963937b0f7ee6a24080834d8fa24c231679948c
Jose <reyesjl.nine@hotmail.com>
2024-02-12 02:12:49 +0000

    add Borrowing class;

---
+++ Borrowing.java
@@ -0,0 +1,36 @@
+import java.util.Date;
+
+public class Borrowing {
+    private Patron patron;
+    private Book book;
+    private Date dueDate;
+
+    public Borrowing(Patron patron, Book book) {
+        this.patron = patron;
+        this.book = book;
+    }
+
+    public Patron getPatron() {
+        return patron;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setPatron(Patron patron) {
+        this.patron = patron;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
